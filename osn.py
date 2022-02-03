9619c782702953f8f032e32d64ac7c5279817f77
aqvi1a <93330418+aqvi1a@users.noreply.github.com>
2022-02-03 18:20:53 +0300

    Update osn.py

--- osn.py
+++ osn.py
@@ -1,17 +1,16 @@
-#там где будет, но пока не работает
-
-
 import pygame
 import requests
 
 FPS = 60
 
-toponym_to_find = '?ll37.769597%2C55.723277&spn=25,25&l=sat'
-geocoder_api_server = "https://static-maps.yandex.ru/1.x/"
-geocoder_params = {
-     "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-     "geocode": toponym_to_find,
-     "format": "json"}
+coords = f'?ll=63.570251%2C12.809027&z=2&spn=10,10l=sat'
+
+geocoder_api_serve = "https://static-maps.yandex.ru/1.x/" + coords
+
+# geocoder_params = {
+#      "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+#      "geocode": toponym_to_find,
+#      "format": "json"}
 
 FULLSCREEN = False
 WIDTH = 600
@@ -26,9 +25,9 @@ def main(fps, width, height, fullscreen=False):
         screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     screen = pygame.display.set_mode((width, height))
 
-    response = requests.get(geocoder_api_server, params=geocoder_params)
+    response = requests.get(geocoder_api_serve)
     if response:
-        with open('Australia.png', 'wb') as f:
+        with open('Australia.jpg', 'wb+') as f:
             f.write(response.content)
 
     running = True
@@ -37,7 +36,7 @@ def main(fps, width, height, fullscreen=False):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        img = pygame.image.load('Australia.png')
+        img = pygame.image.load('Australia.jpg')
         screen.blit(img, (0, 0))
         pygame.display.flip()
     pygame.quit()
