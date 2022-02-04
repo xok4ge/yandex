7b07b3dfaf0881b39a71f6ba41507211153d4237
logeval <loginova032@gmail.com>
2022-02-05 00:10:38 +0300

    add task_3
    add task_3
    fix bug in task_2

--- task_3/main.py
+++ task_3/main.py
@@ -1,10 +1,7 @@
-import task_2.map_dialog as dialog
-from task_2.map_inf import info as inf
+import task_3.map_dialog as dialog
+from task_3.map_inf import info as inf
 
 import pygame
-import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
 
 
@@ -27,7 +24,7 @@ def render(api):
     return img
 
 
-def get_picture(x, y, z=2):
+def get_req(x, y, z=2):
     if -180 < x <= 180 and -81 < y <= 80:
         return f'?ll={x}%2C{y}&z={z}&l=map'
 
@@ -39,11 +36,14 @@ def zoom(z, *args):
         Z = 20
     elif Z < 2:
         Z = 2
-    cords = f'?ll={args[0][0]}%2C{args[0][1]}&z={Z}&l=map'
-    geocoder_api_serv = "https://static-maps.yandex.ru/1.x/" + cords
+    params = f'?ll={args[0][0]}%2C{args[0][1]}&z={Z}&l=map'
+    geocoder_api_serv = "https://static-maps.yandex.ru/1.x/" + params
     img = render(geocoder_api_serv)
     return img
 
+def get_picture(coords):
+    geocoder_api_serve = "https://static-maps.yandex.ru/1.x/" + coords
+    return render(geocoder_api_serve)
 
 def main(fps, width, height, fullscreen=False):
     global always
@@ -54,30 +54,52 @@ def main(fps, width, height, fullscreen=False):
     screen.fill((0, 0, 0))
     dialog.open_dialog()
     polz = inf.polz_i
-    scale = inf.scale_i
+    scale = int(inf.scale_i)
 
     try:
-        coords = get_picture(*polz, scale)
+        coords = get_req(*polz, str(scale))
         if coords is None:
             raise ValueError
         always = polz
     except ValueError:
-        coords = get_picture(*start, scale)
+        coords = get_req(*start, str(scale))
         always = start
-    geocoder_api_serve = "https://static-maps.yandex.ru/1.x/" + coords
-    img = render(geocoder_api_serve)
+    # geocoder_api_serve = "https://static-maps.yandex.ru/1.x/" + coords
+    # img = render(geocoder_api_serve)
     running = True
     while running:
+        zoom_up = False
+        zoom_down = False
         screen.fill((255, 255, 255))
         for event in pygame.event.get():
             keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 running = False
             if keys[pygame.K_PAGEUP]:
-                img = zoom(1, always)
+                zoom_up = True
+                # img = zoom(1, always)
             if keys[pygame.K_PAGEDOWN]:
-                img = zoom(-1, always)
-        screen.blit(img, (0, 0))
+                zoom_down = True
+                # img = zoom(-1, always)
+            if keys[pygame.K_UP]:
+                polz[-1] += screen.get_height() * scale / 200
+            if keys[pygame.K_DOWN]:
+                polz[-1] -= screen.get_height() * scale / 200
+            if keys[pygame.K_RIGHT]:
+                polz[0] += screen.get_width() * scale / 200
+            if keys[pygame.K_LEFT]:
+                polz[0] -= screen.get_width() * scale / 200
+        if zoom_up:
+            screen.blit(zoom(1, always), (0, 0))
+        elif zoom_down:
+            screen.blit(zoom(1, always), (0, 0))
+        else:
+            coords = get_req(*polz, scale)
+            if coords:
+                # print(polz)
+                screen.blit(get_picture(coords), (0, 0))
+            else:
+                print('oops')
         pygame.display.flip()
     pygame.quit()
 
